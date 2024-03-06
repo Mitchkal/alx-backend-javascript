0316fd33287350f8907883dc2012cd0c938606ca
Mitchkal <mitchellkalenda@gmail.com>
2024-03-06 12:44:39 -0500

    Catch me if you can

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,10 +2,7 @@ function handleResponseFromAPI(promise) {
   promise.then(() => {
     console.log('Got a response from the API');
     return ({ status: 200, body: 'success' });
-  }).catch((error) => {
-    console.error('Got an error from the API:', error.message);
-    return new Error();
-  });
+  }).catch(() => new Error());
 }
 
 export default handleResponseFromAPI;
