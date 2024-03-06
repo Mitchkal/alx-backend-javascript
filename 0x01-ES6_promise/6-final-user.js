50a85820a19a7e953bb48a8298fd9bf1910089d1
Mitchkal <mitchellkalenda@gmail.com>
2024-03-06 17:04:19 -0500

    fixed result mapping

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,11 @@ import signupUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const signupPromise = signupUser(firstName, lastName)
-    .then((value) => ({ status: 'fulfilled', value }))
-    .catch((error) => ({ status: 'rejected', value: error }));
-
-  const uploadPromise = uploadPhoto(fileName)
-    .then((value) => ({ status: 'fulfilled', value }))
-    .catch((error) => ({ status: 'rejected', value: error }));
-
-  return Promise.allSettled([signupPromise, uploadPromise]);
+  return Promise.allSettled([signupUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((value) => (
+      value.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
