1c71407cb121f5bf56e05577ad228b77ef90e850
Mitchkal <mitchellkalenda@gmail.com>
2024-03-12 22:18:33 -0400

    handled empty startstring

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
   const filteredValues = Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.substring(startString.length));
