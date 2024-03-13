048aa8ecf2277c7f087bf1f59804edc948cb6cce
Mitchkal <mitchellkalenda@gmail.com>
2024-03-12 22:08:01 -0400

    added dtatview

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8 = new Int8Array(buffer);
+  const dataView = new DataView(buffer);
+
   if (position >= 0 && position < length) {
-    int8[position] = value;
-    return (buffer);
+    dataView.setInt8(position, value);
+    return (dataView);
   }
   throw new Error('Position outside range');
 }
