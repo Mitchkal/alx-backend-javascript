cbcc9c0abe265b6f3defbf9f3d221f228b358954
Mitchkal <mitchellkalenda@gmail.com>
2024-03-14 10:48:49 -0400

    added annotated function to sum list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+modulethat sums a list of floats, annotated
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    calculates sum of list of floats
+    """
+    return sum(input_list)
