0511016a96c8400a3f46d3ec2d6cafc824762a3f
Mitchkal <mitchellkalenda@gmail.com>
2024-04-30 03:31:43 -0400

    added function to be executed by command line with process stdin for user input

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.on("data", (data) => {
+  const name = data.toString().trim();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}\n`);
+  } else {
+    process.stdout.write("Your name is: \n");
+  }
+});
+process.stdin.on("end", () => {
+  process.stdout.write("This important software is now closing\n");
+});
