a3247cc5f880f877a1be06023a036ce9f5b521d9
Mitchkal <mitchellkalenda@gmail.com>
2024-04-30 16:40:50 -0400

    fixed http export

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,9 @@
-const { createServer } = require('node:http');
+const { createServer } = require('http');
 
 const port = 1245;
 
 const app = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
 
