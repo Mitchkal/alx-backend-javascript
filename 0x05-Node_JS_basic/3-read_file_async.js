e544a63e2a92629ebf01a8bbc62167e1bc2c85dd
Mitchkal <mitchellkalenda@gmail.com>
2024-04-30 19:13:55 -0400

    refactored countstudents

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,38 +1,45 @@
-const fs = require('fs/promises');
+const fs = require('fs');
+const util = require('util');
 
-const countStudents = async (path) => {
+const readFileAsync = util.promisify(fs.readFile);
+
+async function countStudents(path) {
   try {
-    const data = await fs.readFile(path, 'utf-8');
-    const lines = data.trim().split('\n');
-    const students = lines.slice(1).map((line) => line.split(','));
+    const data = await readFileAsync(path, 'utf8');
+    const students = data.trim().split('\n').slice(1); // Remove the header line
+
     const fields = {};
+    const studentsByField = {};
 
     students.forEach((student) => {
-      const field = student[3];
-
-      if (field !== undefined && field.trim() !== '') {
+      if (student) {
+        const [firstName, lastName, , field] = student.split(',');
         if (!fields[field]) {
           fields[field] = [];
         }
-        fields[field].push(student[0]);
+        fields[field].push(firstName);
+
+        if (!studentsByField[field]) {
+          studentsByField[field] = [];
+        }
+        studentsByField[field].push(`${firstName} ${lastName}`);
       }
     });
 
-    const totalStudents = students.length;
-    const results = [`Number of students: ${totalStudents}`];
+    console.log(`Number of students: ${students.length}`);
 
-    for (const field in fields) {
-      if (Object.prototype.hasOwnProperty.call(fields, field)) {
-        results.push(
-          `Number of students in ${field}: ${
-            fields[field].length
-          }. List: ${fields[field].join(', ')}`,
-        );
-      }
+    for (const [field, names] of Object.entries(fields)) {
+      console.log(
+        `Number of students in ${field}: ${names.length}. List: ${names.join(
+          ', ',
+        )}`,
+      );
     }
-    console.log(results.join('\n'));
+
+    return students.length;
   } catch (err) {
     throw new Error('Cannot load the database');
   }
-};
+}
+
 module.exports = countStudents;
