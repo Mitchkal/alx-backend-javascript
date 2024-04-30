21ccf5ec7084c852ec733e24c9fb46f6a4f5933b
Mitchkal <mitchellkalenda@gmail.com>
2024-04-30 04:37:01 -0400

    added 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,14 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("readable", () => {
+process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
     const name = chunk.toString().trim();
     if (name) {
       process.stdout.write(`Your name is: ${name}\n`);
-    } else {
-      process.stdout.write("Your name is: \n");
     }
   }
 });
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
