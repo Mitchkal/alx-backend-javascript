8b0b345673a48315d8300f49d3131eca9ee97761
Mitchkal <mitchellkalenda@gmail.com>
2024-04-30 04:34:54 -0400

    incorporated chunk for readbale listener

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,14 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on("readable", () => {
-  const name = process.stdin.read();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    const name = chunk.toString().trim();
+    if (name) {
+      process.stdout.write(`Your name is: ${name}\n`);
+    } else {
+      process.stdout.write("Your name is: \n");
+    }
   }
 });
 process.stdin.on("end", () => {
