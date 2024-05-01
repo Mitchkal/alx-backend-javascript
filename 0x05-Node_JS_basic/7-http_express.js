fdcb32151f898f90e82fece7e1312c163aa09fd5
Mitchkal <mitchellkalenda@gmail.com>
2024-04-30 20:12:01 -0400

    added 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,68 +1,68 @@
-const express = require('express');
-const fs = require('fs/promises');
+const express = require("express");
+const fs = require("fs/promises");
+const util = require("util");
 
-const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
-// const countStudents = require("./2-read_file");
+const readFileAsync = util.promisify(fs.readFile);
+
 const app = express();
-
 const port = 1245;
 
-const countStudents = (path) => new Promise((resolve) => {
-  fs.readFile(path, 'utf-8')
-    .then((data) => {
-      const lines = data.trim().split('\n');
-      const students = lines.slice(1).map((line) => line.split(','));
-      const fields = {};
-
-      students.forEach((student) => {
-        const field = student[3];
-
-        if (
-          field !== undefined
-            && field.trim() !== ''
-            && field !== 'firstname'
-        ) {
-          if (!fields[field]) {
-            fields[field] = [];
-          }
-          fields[field].push(student[0]);
-        }
-      });
-
-      const totalStudents = students.length;
-      const results = [`Number of students: ${totalStudents}`];
-
-      for (const field in fields) {
-        if (Object.prototype.hasOwnProperty.call(fields, field)) {
-          results.push(
-            `Number of students in ${field}: ${
-              fields[field].length
-            }. List: ${fields[field].join(', ')}`,
-          );
-        }
-      }
-      resolve(results.join('\n'));
-    })
-    .catch(() => {
-      throw new Error('Cannot load the database');
-    });
-});
-
-app.get('/', (req, res) => {
-  res.type('text/plain').send('Hello Holberton School!');
-});
-
-app.get('/students', async (req, res) => {
+async function countStudents(path) {
   try {
-    const data = await countStudents(DB_FILE);
-    res.type('text/plain').send(data);
+    const data = await readFileAsync(path, "utf8");
+    const students = data.trim().split("\n").slice(1); // Remove the header line
+
+    const fields = {};
+    const studentsByField = {};
+
+    students.forEach((student) => {
+      if (student) {
+        const [firstName, lastName, , field] = student.split(",");
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstName);
+
+        if (!studentsByField[field]) {
+          studentsByField[field] = [];
+        }
+        studentsByField[field].push(`${firstName} ${lastName}`);
+      }
+    });
+
+    const results = ["This is the list of our students"];
+    results.push(`Number of students: ${students.length}`);
+    for (const [field, names] of Object.entries(fields)) {
+      results.push(
+        `Number of students in ${field}: ${names.length}. List: ${names.join(
+          ", "
+        )}`
+      );
+    }
+
+    return results.join("\n");
+  } catch (err) {
+    throw new Error("Cannot load the database");
+  }
+}
+
+app.get("/", (req, res) => {
+  res.type("text/plain").send("Hello Holberton School!");
+});
+
+app.get("/students", async (req, res) => {
+  const dbFilePath = process.argv.length > 2 ? process.argv[2] : "";
+
+  try {
+    const data = await countStudents(dbFilePath);
+    res.status(200).type("text/plain").send(data).end();
   } catch (error) {
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error").end();
   }
 });
 
 app.listen(port, () => {
-  console.log('Server running at http://127.0.0.1/1245/');
+  console.log(`Server running at http://127.0.0.1:${port}/`);
 });
 
 module.exports = app;
