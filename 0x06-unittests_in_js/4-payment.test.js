cc6860bfb72b5a20016778a542123399d7df3f0d
Mitchkal <mitchellkalenda@gmail.com>
2024-05-02 21:24:21 -0400

    refactored test

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -5,11 +5,10 @@ const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
   it('should call Utils.calculateNumber with correct arguments and display result', () => {
-    const calculateNumberStub = sinon
-      .stub(Utils, 'calculateNumber')
-      .returns(10);
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+    calculateNumberStub.returns(10);
 
-    const consoleLogSpy = sinon.spy(console);
+    const consoleLogSpy = sinon.spy(console, 'log');
     sendPaymentRequestToApi(100, 20);
 
     expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be
